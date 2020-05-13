fa381b1e6adadc3124218da9d04bf66905e052eb
wasanmsn <wasangg@gmail.com>
2020-05-14 04:16:09 +0700

    add lab5-7

--- lab4-angular-app/src/app/app.component.ts
+++ lab4-angular-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	styleData:string;
+	
+	receiveData($event){
+		this.styleData = $event;
+	}
   title = 'lab4-angular-app';
 }
--- lab4-angular-app/src/app/app.module.ts
+++ lab4-angular-app/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { NgStyleComponent } from './components/components/ng-style/ng-style.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { NgForComponent } from './components/ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    NgClassComponent,
+    NgStyleComponent,
+    NgIfComponent,
+    NgSwitchComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
-	ReactiveFormsModule
+	ReactiveFormsModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
